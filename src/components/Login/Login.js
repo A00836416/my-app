7fb8bc425e7e4c182c44626a9acccf87458edcf9
HarolDeveloper <hlancherosalvarez@gmail.com>
2024-09-08 17:54:00 -0600

    Fix

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,7 @@
-
-import React, { useState } from 'react';
-import { login } from '../../services/api';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { login } from '../../services/api';
+import { AuthContext } from '../../App';
 import Button from '../shared/Button/Button';
 import Input from '../shared/InputLogin/Input';
 import styles from './Login.module.css';
@@ -11,11 +11,13 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const navigate = useNavigate();
+    const { setIsAuthenticated } = useContext(AuthContext);
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
             await login(username, password);
+            setIsAuthenticated(true);
             navigate('/home');
         } catch (err) {
             setError('Credenciales inválidas');
@@ -29,36 +31,28 @@ const Login = () => {
                     <h2 className={styles.title}>Login</h2>
                     {error && <p className={styles.errorMessage}>{error}</p>}
                     <form onSubmit={handleLogin}>
-
                         <label htmlFor="username" className={styles.label}>Email</label>
                         <Input
                             type="text"
+                            id="username"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
-                            placeholder="Value"
+                            placeholder="Username"
                         />
-
                         <label htmlFor="password" className={styles.label}>Contraseña</label>
                         <Input
                             type="password"
+                            id="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Value"
+                            placeholder="Password"
                         />
-                        <Button type="submit">Log In</Button>
-
-                        <a href="/link" className={styles.link}>Forgot password?</a>
+                        <Button type="submit">LogIn</Button>
+                        <a href="/forgot-password" className={styles.link}>Forgot password?</a>
                     </form>
-                    
-                        
-                    
-                    
-                    
-                    
                 </div>
-                
             </div>
-            <footer>Need an account?  <a href="/link">Sign up here</a></footer>
+            <footer>Need an account? <a href="/signup">Sign up here</a></footer>
         </div>
     );
 };
