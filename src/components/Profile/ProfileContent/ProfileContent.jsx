e9900ba4f603385d67df992958ad8fe344cb0fb7
dggalvans <dggalvans@gmail.com>
2024-09-23 13:58:07 -0600

    Cambiar nombre de ProfilePic a ProfileHeader en folder y rutas. Crear nuevo archivo de contenido de perfil

---
+++ src/components/Profile/ProfileContent/ProfileContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../ProfileContent/ProfileContent.module.css';
+
+const ProfileContent = () => {
+
+    
+};
+
+export default ProfileContent;
--- src/pages/ProfilePage/profile.jsx
+++ src/pages/ProfilePage/profile.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
 import styles from '../ProfilePage/ProfilePage.module.css';
-import ProfileHeader from '../../components/Profile/ProfileHeader/ProfilePic';
+import ProfileHeader from '../../components/Profile/ProfileHeader/ProfileHeader';
 
 
 const ProfilePage = () => {
     return(
         <div className={styles.ProfileContainer}>
             <ProfileHeader></ProfileHeader>
+
         </div>
 
 
