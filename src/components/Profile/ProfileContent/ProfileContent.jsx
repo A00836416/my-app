7eeb2a582b61e7d95eb968fd8dce88f3ba369307
dggalvans <dggalvans@gmail.com>
2024-09-23 18:49:07 -0600

    Contenido perfil tareas completadas

--- src/components/Profile/ProfileContent/ProfileContent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from '../ProfileContent/ProfileContent.module.css';
-
-const ProfileContent = () => {
-
-    
-};
-
-export default ProfileContent;
--- src/pages/ProfilePage/profile.jsx
+++ src/pages/ProfilePage/profile.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react';
 import styles from '../ProfilePage/ProfilePage.module.css';
 import ProfileHeader from '../../components/Profile/ProfileHeader/ProfileHeader';
+import ProfileTareas from '../../components/Profile/ProfileTareas/ProfileTareas';
 
 
 const ProfilePage = () => {
     return(
         <div className={styles.ProfileContainer}>
             <ProfileHeader></ProfileHeader>
+            <ProfileTareas></ProfileTareas>
 
         </div>
 
