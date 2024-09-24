a023c1439e191a03b9e97e8fc5a6ae36a95357c9
dggalvans <dggalvans@gmail.com>
2024-09-24 01:41:34 -0600

    Contenido perfil logros obtenidos (diseño)

--- src/pages/ProfilePage/profile.jsx
+++ src/pages/ProfilePage/profile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import styles from '../ProfilePage/ProfilePage.module.css';
 import ProfileHeader from '../../components/Profile/ProfileHeader/ProfileHeader';
 import ProfileTareas from '../../components/Profile/ProfileTareas/ProfileTareas';
+import ProfileLogros from '../../components/Profile/ProfileLogros/ProfileLogros';
 
 
 const ProfilePage = () => {
@@ -9,7 +10,7 @@ const ProfilePage = () => {
         <div className={styles.ProfileContainer}>
             <ProfileHeader></ProfileHeader>
             <ProfileTareas></ProfileTareas>
-
+            <ProfileLogros></ProfileLogros>
         </div>
 
 
