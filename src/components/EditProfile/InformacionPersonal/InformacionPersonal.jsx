8ee5693d312b339263cd81b6e8361d3f02754ec8
dggalvans <dggalvans@gmail.com>
2024-10-13 20:48:37 -0600

    Diseño Base de EditProfile, creado los inputs en carpeta InformacionPersonal

--- src/components/EditProfile/InformacionPersonal/InformacionPersonal.jsx
+++ src/components/EditProfile/InformacionPersonal/InformacionPersonal.jsx
@@ -6,45 +6,94 @@ const InformacionPersonal = () => {
     const [nombre, setNombre] = useState('');
     const [apellidoPaterno, setApellidoPaterno] = useState('');
     const [apellidoMaterno, setApellidoMaterno] = useState('');
+    const [correoElectronico, setCorreoElectronico] = useState('');
+    const [genero, setGenero] = useState('');
 
     return (
         <div className={styles.InformacionPersonal}>
             <div className={styles.Title}>
                 <h2>Información Personal</h2>
             </div>
+            
+            <div className={styles.Casillas}>
+                <div className={styles.Nombre}>
+                    <h3 className={styles.label}>Nombre</h3>
+                    <Input
+                        type="text"
+                        id="nombre"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                        placeholder="Nombre"
+                        className={styles.InputNombre}
+                    />
+                </div>
 
-            <div className={styles.Nombre}>
-                <Input
-                    type="text"
-                    id="nombre"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
-                    placeholder="Nombre"
-                    className={styles.InputNombre}
-                />
+                <div className={styles.ApellidoPaterno}>
+                    <h3 className={styles.label}>Appelido Paterno</h3>
+                    <Input
+                        type="text"
+                        id="apellidoPaterno"
+                        value={apellidoPaterno}
+                        onChange={(e) => setApellidoPaterno(e.target.value)}
+                        placeholder="Apellido Paterno"
+                        className={styles.InputPaterno}
+                    />
+                </div>
+
+                <div className={styles.ApellidoMaterno}>
+                    <h3 className={styles.label}>Apellido Materno</h3>
+                    <Input
+                        type="text"
+                        id="apellidoMaterno"
+                        value={apellidoMaterno}
+                        onChange={(e) => setApellidoMaterno(e.target.value)}
+                        placeholder="Apellido Materno"
+                        className={styles.InputMaterno}
+                    />
+                </div>
+
+                <div className={styles.Email}>
+                    <h3 className={styles.label}>Email</h3>
+                    <Input
+                        type="text"
+                        id="email"
+                        value={correoElectronico}
+                        onChange={(e) => setCorreoElectronico(e.target.value)}
+                        placeholder="Email"
+                        className={styles.InputEmail}
+                    />
+                </div>
+
+                <div className={styles.Genero}>
+                    <h3 className={styles.label}>Género</h3>
+                    <div className={styles.generoOptions}>
+                        <label className={styles.generoOption}>
+                            <Input
+                                type="radio"
+                                name="genero"
+                                value="male"
+                                checked={genero === 'male'}
+                                onChange={() => setGenero('male')}
+                            />
+                            <span className={styles.customRadio}></span>
+                            Masculino
+                        </label>
+                        <label className={styles.generoOption}>
+                            <Input
+                                type="radio"
+                                name="genero"
+                                value="female"
+                                checked={genero === 'female'}
+                                onChange={() => setGenero('female')}
+                            />
+                            <span className={styles.customRadio}></span>
+                            Femenino
+                        </label>
+                    </div>
+                </div>
+
             </div>
 
-            <div className={styles.ApellidoPaterno}>
-                <Input
-                    type="text"
-                    id="apellidoPaterno"
-                    value={apellidoPaterno}
-                    onChange={(e) => setApellidoPaterno(e.target.value)}
-                    placeholder="Apellido Paterno"
-                    className={styles.InputPaterno}
-                />
-            </div>
-
-            <div className={styles.ApellidoMaterno}>
-                <Input
-                    type="text"
-                    id="apellidoMaterno"
-                    value={apellidoMaterno}
-                    onChange={(e) => setApellidoMaterno(e.target.value)}
-                    placeholder="Apellido Materno"
-                    className={styles.InputMaterno}
-                />
-            </div>
         </div>
     );
 };
